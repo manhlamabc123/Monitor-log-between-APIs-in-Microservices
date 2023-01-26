f61c4a90f6a0abfe43bee5eac058a1df921fdf79
manh <tranducmanh12092001@gmail.com>
2023-01-26 11:47:31 +0700

    Product API list

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request): #/api/products
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
 
-# Create your views here.
+        return Response(serializer.data)
+
+    def create(self, request): #/api/products
+        pass
+
+    def retrieve(self, request, pk=None): #/api/products/<str:id>
+        pass
+
+    def update(self, request, pk=None): #/api/products/<str:id>
+        pass
+
+    def destroy(self, request, pk=None): #/api/products/<str:id>
+        pass
