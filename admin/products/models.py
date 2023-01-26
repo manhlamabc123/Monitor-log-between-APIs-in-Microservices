99e8fe8d127a0debf1c381d5fcb95ba0f6fad546
manh <tranducmanh12092001@gmail.com>
2023-01-26 11:20:44 +0700

    Create product and user table/model

--- admin/products/models.py
+++ admin/products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=256)
+    image = models.CharField(max_length=256)
+    likes = models.PositiveIntegerField(default=0)
+
+class User(models.Model):
+    pass
