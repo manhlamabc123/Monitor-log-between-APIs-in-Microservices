748b73f38eea0c76ed1c587226c71b0f13fd63ec
manh <tranducmanh12092001@gmail.com>
2023-01-27 16:28:50 +0700

    Data consistency

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -1,5 +1,5 @@
 # amqps://mmbswtkj:86-N4IDgbgw8rOZOsWzJd1tTCA27weth@beaver.rmq.cloudamqp.com/mmbswtkj
-import pika
+import pika, json
 
 params = pika.URLParameters('amqps://mmbswtkj:86-N4IDgbgw8rOZOsWzJd1tTCA27weth@beaver.rmq.cloudamqp.com/mmbswtkj')
 
@@ -7,5 +7,6 @@ connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='main', body='hello main')
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
