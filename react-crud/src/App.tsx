a0422deb5155d167d631ddeb9c9fbaa1f77ae420
manh <tranducmanh12092001@gmail.com>
2023-02-04 14:47:56 +0700

    Update UI

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -1,42 +1,18 @@
 import React from "react";
 import "./App.css";
-import Menu from "./components/Menu";
-import Nav from "./components/Nav";
+import Products from "./admin/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./main/Main";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <div className="container-fluid">
-        <div className="row">
-          <Menu />
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <h2>Section title</h2>
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </main>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/admin/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
