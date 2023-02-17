0960d814e2361c55faf03fa05d28a16de157429a
manh <tranducmanh12092001@gmail.com>
2023-02-17 07:09:16 +0700

    Print out time

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,5 +1,6 @@
 import pika, json
 from rich.pretty import  pretty_repr
+from datetime import datetime
 
 params = pika.URLParameters('amqps://mmbswtkj:86-N4IDgbgw8rOZOsWzJd1tTCA27weth@beaver.rmq.cloudamqp.com/mmbswtkj')
 
@@ -10,29 +11,11 @@ channel = connection.channel()
 channel.queue_declare(queue='log')
 
 def callback(ch, method, properties, body):
-    print('Received in ...')
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Received at:", current_time)
     data = json.loads(body)
     print(pretty_repr(data))
-
-    # if properties.content_type == 'product_created':
-    #     product = Product(id=data['id'], title=data['title'], image=data['image'])
-    #     db.session.add(product)
-    #     db.session.commit()
-    #     print('Product Created')
-
-    # elif properties.content_type == 'product_updated':
-    #     product = Product.query.get(data['id'])
-    #     product.title = data['title']
-    #     product.image = data['image']
-    #     db.session.commit()
-    #     print('Product Updated')
-
-    # elif properties.content_type == 'product_deleted':
-    #     product = Product.query.get(data)
-    #     db.session.delete(product)
-    #     db.session.commit()
-    #     print('Product Deleted')
-
 
 channel.basic_consume(queue='log', on_message_callback=callback, auto_ack=True)
 
